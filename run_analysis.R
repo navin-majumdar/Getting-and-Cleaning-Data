bff405db4532d9281367ff35894bb59cdefcf104
Navin Majumdar <navin.majumdar@gmail.com>
2017-08-20 17:41:13 +0530

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -2,48 +2,66 @@
 library(data.table)
 library(dplyr)
 
-# Load the various datasets and Read data from the files
-test_subject <- read.table("./test/subject_test.txt")
-test_x <- read.table("./test/X_test.txt")
-test_y <- read.table("./test/y_test.txt")
+# Read the data of train
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-train_subject <- read.table("./train/subject_train.txt")
-train_x <- read.table("./train/X_train.txt")
-train_y <- read.table("./train/y_train.txt")
+# Read the data of test
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-features <- read.table("./features.txt")
-activity_labels <- read.table("./activity_labels.txt")
+# Read the features info
+features <- read.table("./UCI HAR Dataset/features.txt")
 
-# Merge the test and train subject datasets
-mysubject <- rbind(test_subject, train_subject)
-colnames(mysubject) <- "subject"
+# Read the activity labels
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-# Merge the test and train labels, applying the textual labels
-label <- rbind(test_y, train_y)
-mylabel <- merge(label, activity_labels, by=1)[,2]
+# Assign column names for the train tables 
+colnames(x_train) <- features[, 2]
+colnames(y_train) <- "activityId"
+colnames(subject_train) <- "subjectId"
 
-# Merge the test and train main dataset, applying the textual headings
-mydata <- rbind(test_x, train_x)
-colnames(mydata) <- features[, 2]
+# Assign column names for the test tables
+colnames(x_test) <- features[, 2]
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
 
-# Merge all three datasets
-finaldata <- cbind(mysubject, mylabel, mydata)
+# Assign column names for activity labels table
+colnames(activity_labels) <- c("activityId", "activityType")
 
-# Rename column
-colnames(finaldata)[2] <- "activity"
+# Merge the training and the test sets to create one data set
+merged_train <- cbind(y_train, subject_train, x_train)
+merged_test <- cbind(y_test, subject_test, x_test)
+mergedData <- rbind(merged_train, merged_test)
 
-# Create a smaller dataset containing only the mean and std variables
-search <- grep("-mean|-std", colnames(finaldata))
-finalsubdata <- finaldata[,c(1,2,search)]
+# Extract only the measurements on the mean and standard deviation for each measurement
+
+# Read column names
+colNames <- colnames(mergedData)
+
+# Create a vector for measurements defining ID, mean and std
+ID_mean_std <- (grepl("activityId", colNames) |
+                 grepl("subjectId", colNames) |
+                 grepl("mean..", colNames) |
+                 grepl("std..", colNames)
+                 )
+# Subset the measurements of mean and std from mergedData
+DataForMeanAndStd <- mergedData[, ID_mean_std == TRUE]
+
+# Use descriptive activity names to name the activities in the data set
+DataWithActivityNames <- merge(DataForMeanAndStd,
+                               activity_labels,
+                               by = 'activityId',
+                               all.x = TRUE
+                               )
 
 # Cleanup column names to remove "() -"
-colnames(finalsubdata) <- gsub("[()-]","",colnames(finalsubdata))
+colnames(DataWithActivityNames) <- gsub("[()-]","",colnames(DataWithActivityNames))
 
-# Compute the means, grouped by subject/label
-averages <- finalsubdata %>% group_by(subject, activity) %>% summarize_all(mean)
+# Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidy_data <- DataWithActivityNames %>% group_by(subjectId, activityId, activityType) %>% summarise_all(mean)
 
 # Save the resulting dataset
-write.table(averages, file = "./tidy_data.txt", row.names = FALSE)
-
-# Output final dataset
-averages
+write.table(tidy_data, file = "./UCI HAR Dataset/tidy_data.txt", row.names = FALSE)
